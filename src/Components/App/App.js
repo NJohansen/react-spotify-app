c2ab7405fd19ede698e117d22769cafc611d9a24
JS&CSS <32328015+NJohansen@users.noreply.github.com>
2018-03-20 20:49:15 +0100

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state.searchResults = {
+      searchResults: [
+        {name: this.props.name,
+        artist: this.props.artist,
+        album: this.props.album},
+        {name: this.props.name,
+        artist: this.props.artist,
+        album: this.props.album},
+
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar/>
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
