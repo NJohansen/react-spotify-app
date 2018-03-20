775b4b9d63755813e33f6f30d16ab847e42cc8a7
JS&CSS <32328015+NJohansen@users.noreply.github.com>
2018-03-21 00:30:36 +0100

    Searchbar interacts with API

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={search:""};
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search(){
+    this.props.onSearch(this.state.search);
+  }
+
+  handleTermChange(event){
+    this.setState({search: event.target.value});
+  }
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <a>SEARCH</a>
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+        <a onClick={this.search}>SEARCH</a>
   </div>
     );
   }
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './SearchResults.css';
+import React from 'react';
 import TrackList from '../TrackList/TrackList';
+import './SearchResults.css';
 
-class SearchResults extends Component {
-  render() {
+class SearchResults extends React.Component{
+  render(){
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <TrackList tracks={this.props.searchResults}/>
-  </div>
+          <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} />
+      </div>
     );
   }
 }
