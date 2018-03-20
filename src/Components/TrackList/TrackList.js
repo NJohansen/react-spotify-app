739a62e6a931f01e2db77c595edf205721da6341
JS&CSS <32328015+NJohansen@users.noreply.github.com>
2018-03-21 00:29:09 +0100

    Tracklist with add/remove options

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import './TrackList.css';
+import React from 'react';
 import Track from '../Track/Track';
+import './TrackList.css';
 
-class TrackList extends Component {
+class TrackList extends React.Component{
   render() {
     return (
       <div className="TrackList">
-        {this.props.track.map()}
+        {this.props.tracks.map(track =>
+          {return(
+            <Track
+            track={track}
+            key= {track.id}
+            onAdd={this.props.onAdd}
+            onRemove={this.props.onRemove}
+            isRemoval={this.props.isRemoval} />)
+          },
+        )}
       </div>
     );
   }
