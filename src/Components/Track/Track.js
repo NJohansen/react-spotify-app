fb365708f7c53c943d2ca12acadf6d0806243d3c
JS&CSS <32328015+NJohansen@users.noreply.github.com>
2018-03-21 00:29:22 +0100

    states added

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,21 +2,38 @@ import React, { Component } from 'react';
 import './Track.css';
 
 class Track extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.state = {isRemoval: false};
+  }
+  removeTrack(){
+    this.props.onRemove(this.props.track);
+  }
+
+  addTrack(){
+    this.props.onAdd(this.props.track);
+  }
+
   renderAction(){
-    if(this.props.isRemoval === true){
-      return '-';
+    if(this.props.isRemoval){
+      return <a className="Track-action" onClick={this.removeTrack}>-</a>
     } else {
-      return '+';
+      return <a className="Track-action" onClick={this.addTrack}>+</a>
     }
   }
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a className="Track-action" isRemoval={true}>{this.renderAction}</a>
+        {this.renderAction()}
       </div>
     );
   }
